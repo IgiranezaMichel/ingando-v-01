37c1da22d3e5fd0eea5dee2115400621a818ae0e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-12 17:07:11 +0200

    add activeCamp

---
+++ src/controller/camp/query.tsx
@@ -0,0 +1,27 @@
+import {useQuery} from '@apollo/client';
+import {PageInput} from '../../types/pageInput';
+import {ACTIVE_CAMP} from '../../graphql/camp/mutation';
+import {useEffect, useState} from 'react';
+import {ResponseData} from '../../types/responseData';
+
+export const useActiveCamp = (page: PageInput) => {
+  const [response, setResponse] = useState<ResponseData>({
+    responseContent: [],
+    responseReady: false,
+  });
+  const {data, refetch} = useQuery(ACTIVE_CAMP, {variables: {page: page}});
+  useEffect(() => {
+    const fetch = async () => {
+      if (data) {
+        return data.activeCamp;
+      }
+    };
+    fetch().then(responseData => {
+      setResponse({
+        responseContent: responseData,
+        responseReady: true,
+      });
+    });
+  }, [data]);
+  return {response, refetch};
+};
