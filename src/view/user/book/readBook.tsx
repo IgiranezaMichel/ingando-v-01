5074482588856bfa14380c71515b011d9f0e62b6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 18:06:40 +0200

    add read book

---
+++ src/view/user/book/readBook.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable eqeqeq */
+/* eslint-disable react/react-in-jsx-scope */
+import {Modal, ScrollView, StatusBar, Text, View} from 'react-native';
+import {sl} from '../../../style';
+import {useModalContext} from '../../../context/modalContext';
+import {useBookLibraryContext} from '../../../context/bookLibraryContext';
+import {useEffect, useState} from 'react';
+import {screen} from '../../../object/screen';
+export const ReadBook = (props: {arrIndex: number}) => {
+  const {isModalVisible, setIsModalVisible} = useModalContext();
+  const {responseContent} = useBookLibraryContext();
+  const [bookData, setBookData] = useState<any>({});
+  useEffect(() => {
+    if (responseContent && responseContent.content != undefined) {
+      setBookData(responseContent.content[props.arrIndex]);
+    }
+  }, [responseContent, props.arrIndex, bookData]);
+  return (
+    <Modal
+      visible={isModalVisible}
+      style={[sl.border0, sl.bgWhite]}
+      transparent
+      animationType="slide">
+      <StatusBar backgroundColor={'white'} hidden />
+      <Text
+        style={[
+          sl.textDark,
+          {position: 'absolute', zIndex: 1},
+          sl.fwBolder,
+          sl.p2,
+          sl.bgMuted,
+          sl.mRight,
+        ]}
+        onPress={() => setIsModalVisible(false)}>
+        close
+      </Text>
+      {bookData != undefined && (
+        <ScrollView style={[sl.card, {height: screen.height}]}>
+          <View style={[sl.card, sl.rounded0]}>
+            <Text style={[sl.textDark]}>BookName {bookData.name}</Text>
+            <Text style={[sl.textDark]}>Author: {bookData.author}</Text>
+          </View>
+        </ScrollView>
+      )}
+    </Modal>
+  );
+};
