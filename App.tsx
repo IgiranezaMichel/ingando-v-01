e7e92070c07de9dcc6a05164977f948ed4aae543
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 17:21:37 +0200

    rearrange tabs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/no-unstable-nested-components */
 import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {Settings} from './src/view/visitor/settings';
 import {Index} from './src/view/visitor';
-import {Login} from './src/view/visitor/authentication/Login';
-import {SignUp} from './src/view/visitor/authentication/Signup';
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {ApolloProvider} from '@apollo/react-hooks';
+import {BookLibrary} from './src/view/visitor/book/bookLibrary';
+import {Image} from 'react-native';
 const client = new ApolloClient({
-  uri: 'http://192.168.43.127:8080/graphql',
+  uri: 'http://10.9.2.67:8080/graphql',
   cache: new InMemoryCache(),
 });
 const Tab = createBottomTabNavigator();
@@ -20,20 +21,77 @@ const App = () => {
         <Tab.Navigator>
           <Tab.Screen
             name="s"
-            options={{headerShown: false}}
+            options={{
+              headerShown: false,
+              tabBarActiveTintColor: 'green',
+              title: 'Camp',
+              tabBarIcon: ({size}) => (
+                <Image
+                  style={{width: size, height: size}}
+                  source={require('./src/assets/camping.png')}
+                />
+              ),
+            }}
             component={Index}
           />
+
           <Tab.Screen
-            name="login"
-            options={{headerShown: false}}
-            component={Login}
+            name="Books"
+            options={{
+              headerShown: true,
+              tabBarActiveTintColor: 'green',
+              tabBarIcon: ({size}) => (
+                <Image
+                  style={{width: size, height: size}}
+                  source={require('./src/assets/book.png')}
+                />
+              ),
+            }}
+            component={BookLibrary}
           />
           <Tab.Screen
-            name="signup"
-            options={{headerShown: false}}
-            component={SignUp}
+            name="Exam"
+            options={{
+              headerShown: true,
+              tabBarActiveTintColor: 'green',
+              tabBarIcon: ({size}) => (
+                <Image
+                  style={{width: size, height: size}}
+                  source={require('./src/assets/exam.png')}
+                />
+              ),
+            }}
+            component={BookLibrary}
           />
-          <Tab.Screen name="Settings" component={Settings} />
+          <Tab.Screen
+            name="History"
+            options={{
+              headerShown: true,
+              tabBarActiveTintColor: 'green',
+              tabBarIcon: ({size}) => (
+                <Image
+                  style={{width: size, height: size}}
+                  source={require('./src/assets/history-book.png')}
+                />
+              ),
+            }}
+            component={BookLibrary}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              title: 'settings',
+              headerShown: true,
+              tabBarActiveTintColor: 'green',
+              tabBarIcon: ({size}) => (
+                <Image
+                  style={{width: size, height: size}}
+                  source={require('./src/assets/setting.png')}
+                />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </ApolloProvider>
