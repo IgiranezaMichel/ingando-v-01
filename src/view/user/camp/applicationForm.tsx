d7a75484becb698f52f6b87374bf870b76f95181
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-17 19:55:04 +0200

    add application form

---
+++ src/view/user/camp/applicationForm.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
+import {Modal, Text, View} from 'react-native';
+import {screen} from '../../../object/screen';
+import {sl} from '../../../style';
+import {ReactNode} from 'react';
+
+export const ApplicationForm = (props: {
+  open: boolean;
+  children: ReactNode;
+}) => {
+  return (
+    <Modal visible={props.open} transparent>
+      <View
+        style={{
+          height: screen.height,
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.737)',
+        }}>
+        <View
+          style={[
+            sl.card,
+            sl.rounded0,
+            sl.colSm11,
+            sl.mAuto,
+            sl.p0,
+            {flexWrap: 'wrap'},
+          ]}>
+          <Text
+            style={[
+              sl.textDark,
+              sl.fwBolder,
+              sl.card,
+              sl.rounded0,
+              sl.colSm12,
+            ]}>
+            Add payment mode used
+          </Text>
+          {props.children}
+        </View>
+      </View>
+    </Modal>
+  );
+};
