98723ae7a8f3ec44d13ed86c60fc16df2ba3a0b4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-17 19:54:53 +0200

    add props

--- src/types/CampApplicantInput.tsx
+++ src/types/CampApplicantInput.tsx
@@ -1,5 +1,11 @@
+import {CampApplicantStatus} from '../enum/campApplicationStatus';
+
 export type CampApplicantInput = {
   id: string;
   campId: string;
   accountHolderId: string;
+  campApplicantStatus: CampApplicantStatus;
+  paymentCode: string;
+  telephone: string;
+  comment: string;
 };
