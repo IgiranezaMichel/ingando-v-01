0e255466b9e5c3f09e47ab749284b927f0510866
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-17 21:03:16 +0200

    make sense

--- src/view/user/camp/index.tsx
+++ src/view/user/camp/index.tsx
@@ -41,7 +41,6 @@ export const Home = () => {
     pageSize: 10,
     sort: 'id',
   });
-  
   const {response, refetch} = useActiveCamp(page);
   const data: DataListApi = {
     content: response,
