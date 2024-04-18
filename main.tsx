2149bdc7b27a4ea596c990d62457c5100ce4e1f2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-18 18:27:19 +0200

    change ip address

--- main.tsx
+++ main.tsx
@@ -3,7 +3,7 @@ import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {ApolloProvider} from '@apollo/react-hooks';
 import AppNavigator from './src/view/navigationContainer';
 const client = new ApolloClient({
-  uri: 'http://10.9.2.140:8080/graphql',
+  uri: 'http://10.9.3.63:8080/graphql',
   cache: new InMemoryCache(),
 });
 export const Main = () => {
