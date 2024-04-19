2a1b5f04f312ea314179f5cbc3c8e064f1603a9c
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-19 12:35:01 +0200

    ScrollView

--- src/view/user/history/historyDetail.tsx
+++ src/view/user/history/historyDetail.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable react-native/no-inline-styles */
 import React, {ReactNode} from 'react';
-import {Image, Modal, Text, View} from 'react-native';
+import {Image, Modal, ScrollView, Text, View} from 'react-native';
 import {sl} from '../../../style';
 import {screen} from '../../../object/screen';
 import {useCampHistoryContext} from '../../../context/campHistoryContext';
@@ -63,14 +63,107 @@ export const HistoryDetail = (props: {
                   ]}
                 />
               </View>
-              <View style={[sl.row]}>
-                <Text style={[sl.textDark, sl.display4, sl.fwBolder]}>
-                  Title :
-                </Text>
-                <Text style={[sl.textDark, sl.display4]}>
-                  {content.responseContent.content[props.arrIndex].camp.title}
-                </Text>
-              </View>
+              <ScrollView>
+                <View style={[sl.mb2, sl.card, sl.colSm12, sl.rounded0]}>
+                  <View style={[sl.row, sl.mb2, sl.rounded0]}>
+                    <Text style={[sl.textDark, sl.display4, sl.fwBolder]}>
+                      Title :
+                    </Text>
+                    <Text style={[sl.textDark, sl.display4]}>
+                      {
+                        content.responseContent.content[props.arrIndex].camp
+                          .title
+                      }
+                    </Text>
+                  </View>
+                  <View
+                    style={[sl.row, sl.mb2, sl.rounded0, {flexWrap: 'wrap'}]}>
+                    <Text style={[sl.textDark, sl.display4, sl.fwBolder]}>
+                      Location :
+                    </Text>
+                    <Text style={[sl.textDark, sl.display4]}>
+                      {
+                        content.responseContent.content[props.arrIndex].camp
+                          .location
+                      }
+                    </Text>
+                    {/*  */}
+                    <View style={[sl.row, sl.mb2, sl.rounded0]}>
+                      <Text style={[sl.textDark, sl.display4, sl.fwBolder]}>
+                        {'  '} cost :
+                      </Text>
+                      <Text style={[sl.textDark, sl.display4]}>
+                        {
+                          content.responseContent.content[props.arrIndex].camp
+                            .cost
+                        }
+                      </Text>
+                    </View>
+                  </View>
+                  <Text style={[sl.mb2, sl.textDark]}>
+                    Status :{' '}
+                    {
+                      content.responseContent.content[props.arrIndex]
+                        .campApplicantStatus
+                    }
+                  </Text>
+                  <Text style={[sl.textDark, sl.display4]}>
+                    {
+                      content.responseContent.content[props.arrIndex].camp
+                        .description
+                    }
+                  </Text>
+                </View>
+                <View style={[sl.row, sl.mb2]}>
+                  <Text style={[sl.textDark, sl.display4, sl.fwBolder]}>
+                    Ay Level :
+                  </Text>
+                  <Text style={[sl.textDark, sl.display4]}>
+                    {
+                      content.responseContent.content[props.arrIndex].levels
+                        .name
+                    }
+                  </Text>
+                </View>
+                <View style={[sl.row]}>
+                  <Text style={[sl.textDark, sl.display4, sl.fwBolder]}>
+                    Age Range :
+                  </Text>
+                  <Text style={[sl.textDark, sl.display4]}>
+                    {
+                      content.responseContent.content[props.arrIndex].levels
+                        .fromAge
+                    }{' '}
+                    -{' '}
+                    {
+                      content.responseContent.content[props.arrIndex].levels
+                        .toAge
+                    }{' '}
+                    years old
+                  </Text>
+                </View>
+                <View style={[{marginBottom: '20%'}]}>
+                  <Text
+                    style={[sl.textShadow, sl.fwBolder, sl.textDark, sl.mt2]}>
+                    Payment
+                  </Text>
+                  <Text style={[sl.fwBolder, sl.textDark, sl.mt2]}>
+                    Phone number{' :'}
+                    {content.responseContent.content[props.arrIndex].telephone}
+                  </Text>
+                  <Text style={[sl.fwBolder, sl.textDark, sl.mt2]}>
+                    Payment code{' :'}
+                    {
+                      content.responseContent.content[props.arrIndex]
+                        .paymentCode
+                    }
+                  </Text>
+                  <Text style={[sl.fwBolder, sl.textDark, sl.mt2]}>
+                    Applied on{' :'}
+                    {content.responseContent.content[props.arrIndex].timeStamp}
+                  </Text>
+                </View>
+              </ScrollView>
             </View>
           )}
       </View>
