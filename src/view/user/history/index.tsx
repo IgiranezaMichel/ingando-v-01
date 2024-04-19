251a2ccbdb9bed6c467d8c76c3fe9c42f2744d03
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-19 08:43:24 +0200

    add context

--- src/view/user/history/index.tsx
+++ src/view/user/history/index.tsx
@@ -1,13 +1,38 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable eqeqeq */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/react-in-jsx-scope */
 import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import {sl} from '../../../style';
 import {useLogoutModal} from '../../../context/logoutContext';
+import {useDisplayCampHistoryPage} from '../../../controller/campApplicant/query';
+import {useState} from 'react';
+import {PageInput} from '../../../types/pageInput';
+import {useAccountHolderContext} from '../../../context/accountHolderContext';
+import {HistoryDetail} from './historyDetail';
+import {DataListApi} from '../../../types/dataListApi';
+import {CampHistoryContext} from '../../../context/campHistoryContext';
 
 export const History = () => {
+  const [openDetailModal, setOpenDetailModal] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   const {setShowLogoutModal} = useLogoutModal();
+  const [page] = useState<PageInput>({
+    pageNumber: 0,
+    pageSize: 10,
+    sort: 'id',
+  });
+  const {responseContent} = useAccountHolderContext();
+  const {response} = useDisplayCampHistoryPage(page, responseContent.email);
+  const data: DataListApi = {
+    content: response,
+    updateContent() {
+      response.refresh();
+    },
+  };
   return (
-    <>
+    <CampHistoryContext.Provider value={data}>
       <View style={[{backgroundColor: 'blue'}]}>
         <Text
           style={[
@@ -55,14 +80,49 @@ export const History = () => {
         </TouchableOpacity>
       </View>
       <ScrollView>
-        <View style={[sl.card, sl.rounded0, sl.mt2, sl.colSm11_5, sl.mAuto]}>
-          <Text style={[sl.textDark, sl.fwBolder, sl.display3]}>Header</Text>
-          <View style={[sl.row]}>
-            <Text style={[sl.textDark]}>Location</Text>
-            <Text style={[sl.textDark, sl.mx4]}>Cost</Text>
-          </View>
-        </View>
+        {response.responseReady &&
+          response.responseContent != undefined &&
+          response.responseContent.content != undefined &&
+          response.responseContent.content.length != 0 &&
+          response.responseContent.content.map((data: any, index: number) => {
+            return (
+              <View
+                key={data.id}
+                style={[sl.card, sl.rounded0, sl.mt2, sl.colSm11_5, sl.mAuto]}>
+                <Text style={[sl.textDark, sl.fwBolder, sl.display4]}>
+                  {data.camp.title}
+                </Text>
+                <Text style={[sl.textDark]}>Ay level :{data.levels.name}</Text>
+                <Text style={[sl.textPrimary]}>
+                  status :{data.campApplicantStatus}
+                </Text>
+                <View style={[sl.row]}>
+                  <Text style={[sl.textDark]}>
+                    Location:{data.camp.location}
+                  </Text>
+                  <Text style={[sl.textDark, sl.mx4]}>
+                    Cost: {data.camp.cost}
+                  </Text>
+                </View>
+                <Text
+                  onPress={() => {
+                    setSelectedIndex(index);
+                    setOpenDetailModal(true);
+                  }}
+                  style={[sl.textDark, sl.mRight, sl.fwBolder]}>
+                  Detail
+                </Text>
+              </View>
+            );
+          })}
+        {openDetailModal && (
+          <HistoryDetail arrIndex={selectedIndex} open={openDetailModal}>
+            <View style={[sl.colSm12]}>
+              <Text>x</Text>
+            </View>
+          </HistoryDetail>
+        )}
       </ScrollView>
-    </>
+    </CampHistoryContext.Provider>
   );
 };
