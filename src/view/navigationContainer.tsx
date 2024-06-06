db2407669e8f3a29824aa9b17e23b0f193b8d3bd
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-06 16:05:53 +0200

    add LoginProvider

--- src/view/navigationContainer.tsx
+++ src/view/navigationContainer.tsx
@@ -5,32 +5,35 @@ import {Login} from './visitor/authentication/Login';
 import AuthenticatedUserBottomNavigation from './user/userBottomNavigation';
 import {Index} from './Index';
 import {ForgetPassword} from './visitor/authentication/forgetPassword';
+import {LoginProvider} from './visitor/authentication/loginProvider';
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Home_component"
-          component={Index}
-        />
-        <Stack.Screen
-          options={{headerShown: false, animation: 'flip'}}
-          name="login"
-          component={Login}
-        />
-        <Stack.Screen
-          options={{headerShown: false, animation: 'slide_from_bottom'}}
-          name="forgetPassword"
-          component={ForgetPassword}
-        />
-        <Stack.Screen
-          options={{headerShown: false, animation: 'simple_push'}}
-          name="auth_user"
-          component={AuthenticatedUserBottomNavigation}
-        />
-      </Stack.Navigator>
+      <LoginProvider>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Home_component"
+            component={Index}
+          />
+          <Stack.Screen
+            options={{headerShown: false, animation: 'flip'}}
+            name="login"
+            component={Login}
+          />
+          <Stack.Screen
+            options={{headerShown: false, animation: 'slide_from_bottom'}}
+            name="forgetPassword"
+            component={ForgetPassword}
+          />
+          <Stack.Screen
+            options={{headerShown: false, animation: 'simple_push'}}
+            name="auth_user"
+            component={AuthenticatedUserBottomNavigation}
+          />
+        </Stack.Navigator>
+      </LoginProvider>
     </NavigationContainer>
   );
 };
