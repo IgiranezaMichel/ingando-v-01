1bc937037e977b641644f8aa25cc09fc46860714
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-06 16:05:42 +0200

    add sl

--- src/view/Index.tsx
+++ src/view/Index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/react-in-jsx-scope */
 import {
@@ -11,25 +12,39 @@ import {
 } from 'react-native';
 import { screen } from '../object/screen';
 import { useNavigation } from '@react-navigation/native';
+import { sl } from '../style';
+import LottieView from 'lottie-react-native';
 export const Index = () => {
     const navigation = useNavigation();
     return (
         <ScrollView style={styles.container}>
             <StatusBar backgroundColor={'blue'} />
             <View style={styles.logoContainer}>
+                <View style={[sl.card,sl.roundedCircle,sl.dFlex,sl.justifyContentCenter,sl.alignItemCenter,{paddingLeft:30,paddingRight:30}]}>
                 <Image
                     style={styles.logo}
                     source={require('../../src/assets/visitor/download.png')}
                 />
+                </View>
+                <Text style={[styles.textDescription,sl.textShadow,sl.textWhite]}>
+                        Welcome to Adventist Youth App
+                </Text>
+                <Text style={[sl.textWhite]}>
+                        This system is designed to help adventist Youth
+                </Text>
             </View>
             <View style={styles.btnOptionsContainer}>
                 <View>
-                    <Text style={styles.textDescription}>
-                        Welcome to Adventist Youth App
+                <LottieView
+                            autoPlay={true}
+                            source={require('../assets/lotties/down.json')}
+                            style={[sl.mAuto,{width: 90, height: 90}]}
+                          />
+                    <Text style={[sl.textCenter,sl.textWhite,sl.fwBolder]}>
+                        CLick button below to login 
                     </Text>
-                    <Text style={styles.ingando}>Ingando Management System</Text>
                     <TouchableWithoutFeedback onPress={() => {navigation.navigate('login' as never);}}>
-                        <View style={styles.loginBtn}>
+                        <View style={[styles.loginBtn,sl.mt8,sl.rounded1]}>
                             <Text style={styles.loginTxt}>Login</Text>
                         </View>
                     </TouchableWithoutFeedback>
@@ -43,22 +58,16 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(240, 248, 255, 0.667)',
     },
     btnOptionsContainer: {
-        transform: [{ translateY: -(screen.height / 2) * 0.27 }],
-        backgroundColor: 'white',
+        transform: [{ translateY: -(screen.height / 1.6) * 0.27 }],
         marginLeft: screen.width * 0.07,
         marginRight: screen.width * 0.07,
-        borderColor: 'black',
-        shadowColor: '#000',
-        shadowOpacity: 0.9,
-        shadowRadius: 5,
-        elevation: 10,
         paddingTop: 30,
         paddingBottom: 40,
         borderRadius: 10,
     },
     logoContainer: {
         backgroundColor: 'blue',
-        height: screen.height / 2,
+        height: screen.height /1.5,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -79,7 +88,7 @@ const styles = StyleSheet.create({
     loginBtn: {
         borderColor: 'black',
         backgroundColor: 'green',
-        width: screen.width * 0.8,
+        width: screen.width * 0.3,
         alignSelf: 'center',
     },
     ingando: {
