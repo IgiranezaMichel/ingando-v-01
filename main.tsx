19886648f11c65a89da1a8e1008260fb81528ebd
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-09 21:29:47 +0200

    change api

--- main.tsx
+++ main.tsx
@@ -3,7 +3,7 @@ import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {ApolloProvider} from '@apollo/react-hooks';
 import AppNavigator from './src/view/navigationContainer';
 const client = new ApolloClient({
-  uri: 'http://172.20.2.165:8080/graphql',
+  uri: 'http://192.168.20.39:8080/graphql',
   cache: new InMemoryCache(),
 });
 export const Main = () => {
